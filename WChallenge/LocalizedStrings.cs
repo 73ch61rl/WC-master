d38213ef3b245a7d361d6b2f45df3921aeb6f39f
ola <isdevpro@gmail.com>
2013-06-15 00:28:20 +0300

    localiz

---
+++ WChallenge/LocalizedStrings.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WChallenge
+{
+    public class LocalizedStrings
+    {
+        public LocalizedStrings()
+        {
+        }
+
+        private static AppResources localizedResources = new AppResources();
+
+        public AppResources AppResources
+        {
+            get { return localizedResources; }
+        }
+
+    }
+
+}
