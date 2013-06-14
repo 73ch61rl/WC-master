94617593d63942ecbbbfdfc80470fdda60c42b19
jalentheory <jhilum@hotmail.com>
2013-06-14 14:43:48 +0300

    TreeHelper added and some vhanges

--- WChallenge/MainPage.xaml.cs
+++ WChallenge/MainPage.xaml.cs
@@ -31,7 +31,7 @@ namespace WChallenge
     public partial class MainPage : PhoneApplicationPage
     {
         // Constructor
-       
+
         public MainPage()
         {
             InitializeComponent();
@@ -40,38 +40,44 @@ namespace WChallenge
             DataContext = App.ViewModel;
             if (!NetworkInterface.GetIsNetworkAvailable())
             {
-              MessageBoxResult m =  MessageBox.Show("You arent connected to the internet to load the techniques. Want to connect now?", "No connection", MessageBoxButton.OKCancel);
-                if(m==MessageBoxResult.OK){
+                MessageBoxResult m = MessageBox.Show("You arent connected to the internet to load the techniques. Want to connect now?", "No connection", MessageBoxButton.OKCancel);
+                if (m == MessageBoxResult.OK)
+                {
                     ConnectionSettingsTask connectionSettingsTask = new ConnectionSettingsTask();
                     connectionSettingsTask.ConnectionSettingsType = ConnectionSettingsType.WiFi;
                     connectionSettingsTask.Show();
                 }
                 if (m == MessageBoxResult.Cancel)
                 {
-                    
-                }
-               
 
-
+                } 
             }
+
+TreeHelper treeHelper = new TreeHelper();
+ for (int i = 0; i < TechnicListBox.Items.Count(); i++)
+{
+ var currentSelectedListBoxItem = this.TechnicListBox.ItemContainerGenerator.ContainerFromIndex(i) as ListBoxItem;
+CharacterProgressControl.WP8.CProgressControl cp = treeHelper.FindDescendant<CharacterProgressControl.WP8.CProgressControl>(currentSelectedListBoxItem);
+} 
+ 
             this.Loaded += new RoutedEventHandler(MainPage_Loaded);
         }
-         
+
         // Load data for the ViewModel Items
         private void MainPage_Loaded(object sender, RoutedEventArgs e)
         {
-           
+
             if (!App.ViewModel.IsDataLoaded)
             {
-                App.ViewModel.LoadData(); 
+                App.ViewModel.LoadData();
             }
         }
 
         protected override void OnNavigatedTo(System.Windows.Navigation.NavigationEventArgs e)
         {
-          
 
-           //MessageBox.Show("MAINPAGE= "+Convert.ToString(App.ViewModel.Items[0].percentageDone));
+
+            //MessageBox.Show("MAINPAGE= "+Convert.ToString(App.ViewModel.Items[0].percentageDone));
         }
 
 
@@ -79,16 +85,16 @@ namespace WChallenge
         {
             if (TechnicListBox.SelectedIndex == -1)
                 return;
-                TechniqueViewModel item = new TechniqueViewModel();
-                item = TechnicListBox.SelectedItem as TechniqueViewModel;
-                NavigationService.Navigate(new Uri("/Technique.xaml?TechniqueId=" + item.Id, UriKind.Relative));
-            
+            TechniqueViewModel item = new TechniqueViewModel();
+            item = TechnicListBox.SelectedItem as TechniqueViewModel;
+            NavigationService.Navigate(new Uri("/Technique.xaml?TechniqueId=" + item.Id, UriKind.Relative));
+
             TechnicListBox.SelectedIndex = -1;
 
-            
+
         }
 
-        
+
 
         private void fight_manuervers_Button_Click_1(object sender, RoutedEventArgs e)
         {
@@ -131,7 +137,7 @@ namespace WChallenge
 
         private void header_Tap(object sender, System.Windows.Input.GestureEventArgs e)
         {
-            MessageBox.Show("Kiai! (kee-yai) is an app that helps you to memorize self defence tactics that is vital for all to know. The tactics are optimized for females general body index.","About Kiai",MessageBoxButton.OK);
+            MessageBox.Show("Kiai! (kee-yai) is an app that helps you to memorize self defence tactics that is vital for all to know. The tactics are optimized for females general body index.", "About Kiai", MessageBoxButton.OK);
         }
 
         private void settinsTap_Tap(object sender, System.Windows.Input.GestureEventArgs e)
@@ -149,8 +155,8 @@ namespace WChallenge
 
             NavigationService.Navigate(new Uri("/Settings.xaml", UriKind.Relative));
         }
-        
- 
+
+
 
 
 
